47bed1f3c24466f0b67400748a8f1aa71640a7b0
Herbert Schilling <hschilling@nasa.gov>
2022-10-16 11:00:26 -0400

    Removed more unused code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,23 +16,6 @@ public class Constants {
         public static final int LEFT_FRONT_MOTOR_ID = 4;
         public static final int LEFT_BACK_MOTOR_ID = 3;
 
-        //solenoids
-        public static final int SHIFT_HIGH_SPEED_SOLENOID_PORT = 0;
-	    public static final int SHIFT_HIGH_TORQUE_PORT= 1;
-        public static final int PCM_ADDRESS = 3;
-        public static final double SHIFT_TIMER = 0.5;
-
-        //follow vision target cmd speeds
-        public static final double TARGET_FOLLOWING_SPEED = 0.5;
-        public static final double BUTTERY_FOLLOWING_SPEED = 0.035;
-
-        // encoder 
-       public static final double HIGH_TORQUE_REVOLUTION_TO_INCH_CONVERSION = 2.317175948;
-       public static final double HIGH_SPEED_REVOLUTION_TO_INCH_CONVERSION = 1.048247093;
-
-
-        public static final double TURN_TO_HUB_KP = 0.005;
-
         // No turning sensitivity
         public static final double MAX_TURN_SPEED = 1;
 
@@ -46,25 +29,6 @@ public class Constants {
 
         public static final int FORWARD_JOYSTICK_INVERT = 1;    
         public static final int TURN_JOYSTICK_INVERT = 1;
-      
-
-        public static final int INTAKE_ARM_RETRACT = 11; 
-        public static final int INTAKE_ARM_EXTEND = 12; 
-        //public static final int INTAKE_BACK = 6;
-
-        public static final int SHOOTER_BTN = 3; 
-
-        //public static final int INDEXER = 9;
-
-        public static final int INDEXER_FWD = 6;
-        public static final int INDEXER_BACK = 4;
-
-        // public static final int TURN_TO_N = 12; // get rid of this eventually
-
-        public static final int CLIMBER_UP = 5;
-        public static final int CLIMBER_DOWN = 3;
-
-        public static final int MAX_SHOOT = 7;
 
     } 
 
@@ -100,20 +64,6 @@ public class Constants {
         public static final int FORWARD_JOYSTICK_INVERT = 1;
         public static final int TURN_JOYSTICK_INVERT = 1;
 
-        public static final int TURN_RIGHT = XboxMappingToJoystick.B_BUTTON;
-        public static final int TURN_LEFT = XboxMappingToJoystick.X_BUTTON;
-        public static final int TURN_180 = XboxMappingToJoystick.A_BUTTON;
-        public static final int SHIFT_HIGH_SPEED = XboxMappingToJoystick.RIGHT_BUMPER; 
-        public static final int SHIFT_HIGH_TORQUE = XboxMappingToJoystick.LEFT_BUMPER ; 
-        public static final int INTAKE = XboxMappingToJoystick.LEFT_TRIGGER; 
-        public static final int INDEX_AND_SHOOT = XboxMappingToJoystick.RIGHT_TRIGGER;
-        public static final int POINT_AND_SHOOT = XboxMappingToJoystick.A_BUTTON;
-        public static final int TURN_RIGHT_90_CCW = XboxMappingToJoystick.B_BUTTON;
-        public static final int TURN_RIGHT_90_CW = XboxMappingToJoystick.X_BUTTON;
-        public static final int SPIT_OUT_BALL = XboxMappingToJoystick.Y_BUTTON;
-
-
-
         public static final double FORWARD_DEADBAND = 0.05;
         public static final double TURN_DEADBAND = 0.1;
 
@@ -122,97 +72,7 @@ public class Constants {
 
         public static final double JOYSTICK_SENSITIVITY = 0.5;
     }
-
-    public static final class IntakeConstants {
-        public static final int INTAKE_MOTOR_ID = 1;
-        public static final double INTAKESPEED = 0.75;
-        public static final double INTAKE_SLEW = 5;
-
-        //intake arm
-        public static final int EXTEND_INTAKE_ARM = 2;
-        public static final int RETRACT_INTAKE_ARM = 3;
-    }
-
-    public static final class ShooterConstants {
-        public static final int PID_IDX = 0;
-        public static final int CAN_TIMEOUT = 10;
-        public static final int TOP_MOTOR_ID = 10; 
-        public static final int BOTTOM_MOTOR_ID = 9;
-
-        public static final double SHOOTER_SPEED = 0.06;
- 
-        public static final double SETPOINT_RATIO = 2.7;
-        public static final double BOTTOM_SETPOINT = 2100;
-        public static final double TOP_SETPOINT = BOTTOM_SETPOINT * SETPOINT_RATIO;
-        public static final double MAX_SETPOINT = 5000;
-
-        public static final double FENDER_LOWER_SHOOTER_TOP_SPEED = 900;
-        public static final double FENDER_LOWER_SHOOTER_BOTTOM_SPEED = 850;
-
-        // public static final double FENDER_LOWER_SHOOTER_TOP_SPEED = 1100;
-        // public static final double FENDER_LOWER_SHOOTER_BOTTOM_SPEED = 650;
-
-        public static final double FENDER_UPPER_SHOOTER_TOP_SPEED = 575 * 1.1;
-        public static final double FENDER_UPPER_SHOOTER_BOTTOM_SPEED = 2875 * 1.1;
-
-        public static final double TARMAC_UPPER_SHOOTER_TOP_SPEED = 1198;
-        public static final double TARMAC_UPPER_SHOOTER_BOTTOM_SPEED = 2995;
-
-        public static final double MIDDLE_AUTON_TARMAC_UPPER_SHOOTER_TOP_SPEED = 907.5;
-        public static final double MIDDLE_AUTON_TARMAC_UPPER_SHOOTER_BOTTOM_SPEED = 3025;
-
-        public static final double SHOOTER_KP = .0003;//1.875;
-        public static final double SHOOTER_KI = 0.000001;//0.006;
-        // public static final double SHOOTER_KI = 1;//0.006;
-        //0.000001
-        public static final double SHOOTER_KD = 0.0003;//52.5;
-        public static final double SHOOTER_KF = 0.00023; //0.15;
-        public static final double SHOOTER_KIZ = 250;
-        // public static final double SHOOTER_KIZ = 0;
-
-        //250
-        public static final double SHOOTER_K_MAX_OUTPUT = 1;
-        public static final double SHOOTER_K_MIN_OUTPUT = 0;
-        public static final double SHOOTER_MAX_RPM = 5700;
-    
-        public static final double SHOOTER_MAX_ACCEL = 0;
-    }
-
-    public static final class IndexerConstants {
-        public static final int INDEXER_MOTOR_ID = 2;
-        public static final double INDEXERSPEED = 0.5;
-        public static final int LIMIT_SWITCH_ID = 0;
-        
-        public static final int INDEXER_SLEW = 5;
-    }
-
-    // public static final class PrototypeDriveConstants {
-    //     public static final int LEFT_FRONT_MOTOR_ID = 8;
-    //     public static final int LEFT_MIDDLE_MOTOR_ID = 7;
-    //     public static final int LEFT_BACK_MOTOR_ID = 6;
-
-    // }
   
-    public static final class ClimberConstants {
-        public static final int LEFT_CLIMBER_MOTOR_ID = 12;
-        public static final int RIGHT_CLIMBER_MOTOR_ID = 11;
-        public static final double CLIMBER_SPEED = 0.5;
-        public static final double MAX_HEIGHT = 0.9;
-        public static final double MIN_HEIGHT = 0;
-
-        public static final int CLIMBER_SLEW = 5;
-
-    }
-
-
-    public static final class PhotonLimelightConstants {
-        public static final double CAMERA_HEIGHT_INCHES = 38;
-        public static final double TARGET_HEIGHT_INCHES = 104;
-        public static final double HUB_RADIUS_INCHES = 27;
-        public static final double TILT_DEGREES = 36;
-        // public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
-        // public static final double GOAL_RANGE_METERS = Units.feetToMeters(3);
-    }
 }
 
 
